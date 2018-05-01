4889856c58444f3155a8bf4abc84473d404e2f35
Dani Lesmiadi <dani@foodgasm.id>
2018-05-01 18:47:49 +0700

    Refactoring Jenis Saprotan

--- resources/views/jenis_saprotans/show.blade.php
+++ resources/views/jenis_saprotans/show.blade.php
@@ -9,16 +9,21 @@
     <div class="content">
         <div class="box box-warning">
             <div class="box-body">
-                <div class="row" style="padding-left: 20px">
-                    @include('jenis_saprotans.show_fields')
-                    <a href="{!! route('jenisSaprotans.index') !!}" class="btn btn-default">
-                        <i class="fa fa-arrow-left"></i>
-                    </a>
-                    <a href="{!! route('jenisSaprotans.edit', $jenisSaprotan->id) !!}" class="btn btn-warning">
-                        <i class="fa fa-edit"></i>
-                    </a>
+                <div class="row">
+                    <div class="col-md-12">
+                        @include('jenis_saprotans.show_fields')
+                    </div>
                 </div>
-
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="{!! route('jenisSaprotans.index') !!}" class="btn btn-default">
+                            <i class="fa fa-arrow-left"></i>
+                        </a>
+                        <a href="{!! route('jenisSaprotans.edit', $jenisSaprotan->id) !!}" class="btn btn-warning pull-right">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
